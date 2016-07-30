fc60442ba1aa03d7e7974910cdf962214305c0de
cnspraveen <cns.praveen@gmail.com>
2016-07-30 19:36:06 +0530

    Update SampleClass.java

--- src/com/git/hub/SampleClass.java
+++ src/com/git/hub/SampleClass.java
@@ -5,6 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) 
 	{
 		System.out.println("Moving to Github");
+		System.out.println("Moving to Github & import back this updated program in Eclipse");
 	}
 
 }
